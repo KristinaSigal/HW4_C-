1c19c433dbfc68f0945a66e67c90cc242ec93c0e
kristina.sigal <kulrikh@gmail.com>
2022-08-22 00:49:41 +0300

    Add firth commit_save HW4_new folder

---
+++ Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который 
+// 1. принимает на вход два числа (A и B) и 
+// 2. возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b;
+do {
+    Console.WriteLine("Введите второе натуральное число");
+    b = Convert.ToInt32(Console.ReadLine());
+    }
+    while (b < 0);
+
+int rate = 1;
+for (int i = 0; i < b; i++)
+{
+    rate = rate*a;
+}
+
+Console.WriteLine($"Число {a} в степени {b} равно {rate}.");
---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int number)
+{
+    int sum = 0;
+    while (number >= 10)
+    {
+        sum = number % 10 + sum;
+        number = number / 10;
+    }
+    return sum + number;
+}
+Console.WriteLine($"Сумма цифр в числе {num} равна {SumDigit(num)}.");
